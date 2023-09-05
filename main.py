e45bbf263ccec9fea1d5a839868ea25485ae0a20
gqbrofree <143730629+gqbrofree@users.noreply.github.com>
2023-09-06 00:13:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,22 +9,19 @@ new Env('机场通用签到');
 Update: 2023/8/31
 """
 
-
 import requests, json, re, os, json
+import checksendNotify
 
 session = requests.session()
 # 机场的地址
-url = os.environ.get('URL')
+url = os.environ.get('JC_URL')
 # 配置用户名（一般是邮箱）
-email = os.environ.get('EMAIL')
+email = os.environ.get('JC_EMAIL')
 # 配置用户名对应的密码 和上面的email对应上
-passwd = os.environ.get('PASSWD')
-# server酱
-SCKEY = os.environ.get('SCKEY')
+passwd = os.environ.get('JC_PASSWD')
 
 login_url = '{}/auth/login'.format(url)
 check_url = '{}/user/checkin'.format(url)
-
 
 header = {
         'refere': login_url,       
@@ -39,40 +36,26 @@ data = {
         'remember_me': False
 }
 
-
 #进行登录
-
 print('进行登录..'+login_url)
 
-
 try:       
-
     response = json.loads(session.post(url=login_url,headers=header,data=json.dumps(data)).text) 
-        
     print(response['msg'])
 
 except:
-    print('录失败，请检查网络或参数')   
+    print('登录失败，请检查网络或参数')   
 
-# 进行签到
+# 签到
 print('进行签到..'+check_url)
 
-
-try:
-          
-
+try:          
     result = json.loads(session.post(url=check_url,headers=header).text)
     print(result['msg'])
     content = result['msg']
-       
-    # 进行推送
-    if SCKEY != '':
-        push_url = 'https://sctapi.ftqq.com/{}.send?title=机场签到&desp={}'.format(SCKEY, content)
-        requests.post(url=push_url)
-        print('推送成功')
+    
+    # 推送
+    checksendNotify.send(url+"签到成功", content)
 except:
-    content = '签到失败'
-    print(content)
-    if SCKEY != '':
-        push_url = 'https://sctapi.ftqq.com/{}.send?title=机场签到&desp={}'.format(SCKEY, content)
-        requests.post(url=push_url)
+    print('签到失败，检查URL、用户密码！')
+    
